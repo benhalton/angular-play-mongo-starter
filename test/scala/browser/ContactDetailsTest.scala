b38103910abc476d2b726e1efc320a3c0d220507
benhalton <ben@algorithmica.co.uk>
2015-02-17 22:06:15 +0000

    run browser tests against local phantom

--- test/scala/browser/ContactDetailsTest.scala
+++ test/scala/browser/ContactDetailsTest.scala
@@ -1,15 +1,29 @@
 package scala.browser
 
-import play.api.test.{FakeApplication, WithBrowser}
+import org.openqa.selenium.phantomjs.PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY
+import org.openqa.selenium.phantomjs.PhantomJSDriver
+import org.openqa.selenium.remote.DesiredCapabilities
+import play.api.test.{TestBrowser, FakeApplication, WithBrowser}
 import org.openqa.selenium.Dimension
 import org.fest.assertions.Assertions._
+
 class ContactDetailsTest extends BrowserSpecification with EmbeddedMongo {
 
   class WithWSETSAT extends WithBrowser(PHANTOM, FakeApplication(additionalConfiguration = inMemoryMongoDatabase())) {}
 
+  class WithPhantomJs extends WithWSETSAT {
+    lazy override val browser: TestBrowser = {
+      val defaultCapabilities = DesiredCapabilities.phantomjs
+      val additionalCapabilities = new DesiredCapabilities()
+      additionalCapabilities.setCapability(PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "node_modules/phantomjs/bin/phantomjs")
+      val capabilities = new DesiredCapabilities(defaultCapabilities, additionalCapabilities)
+      val driver = new PhantomJSDriver(capabilities)
+      TestBrowser(driver, Some("http://localhost:" + port))
+    }
+  }
 
-  "run in a browser" in new WithWSETSAT {
 
+  "run in a browser" in new WithPhantomJs {
     browser.manage.window.setSize(new Dimension(1080, 600))
     browser.goTo("/")
     assertThat(browser.title()).isEqualTo("WSET Systematic Approach to Tasting")
